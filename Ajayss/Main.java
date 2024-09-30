f9948de19f6f8314549f307623c29d4c90964fd6
Ajay Dhiman <ajaydhiman09012002@gmail.com>
2024-09-30 15:55:14 +0530

    [Ajay first commit]

---
+++ Ajayss/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+public static void main(String args[]){
+int a = 10;
+int b = 50;
+int sum = a+b;
+System.out.println(sum);
+}
+}
+
+question: what is class?
+answer : class is a tamplate.
+
+
